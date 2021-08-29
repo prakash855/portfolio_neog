896693ed84d097dc253181a905165dfe901e3bd9
Prakash Agrahari <sshivamprakashh301@gmail.com>
2021-08-29 22:46:47 +0530

    footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,11 +1,63 @@
-import React from 'react'
+import React from "react";
+import "./Footer.css";
+import { HeartFilled } from "@ant-design/icons";
 
+import code from "../../images/code.png";
+import github from "../../images/github.gif";
+import heart from "../../images/heart.gif";
+import linkedin from "../../images/linkedin.gif";
+import medium from "../../images/medium.png";
+import twitter from "../../images/twitter.gif";
+import Prakash from '../../images/prakash.gif'
 const Footer = () => {
-    return (
-        <div>
-            
+  return (
+    <footer className="footer">
+      <div className="foot">
+        <div id="prakash">
+          <img
+            style={{ width: "5rem", borderRadius: "10px" }}
+            src={Prakash}
+            alt=""
+          />
         </div>
-    )
-}
+        <ul className="lists">
+          <a
+            href="https://www.linkedin.com/in/prakash-agrahari-a5655312b/"
+            target="#"
+          >
+            <li>
+              <img className="list" src={linkedin} alt="" />
+            </li>
+          </a>
+          <a href="https://github.com/prakash855" target="#">
+            <li>
+              <img className="list" src={github} alt="" />
+            </li>
+          </a>
+          <a href="https://twitter.com/PrakashAgrahar3" target="#">
+            <li>
+              <img className="list" src={twitter} alt="" />
+            </li>
+          </a>
+          <a href="https://prakash855.medium.com/" target="#">
+            <li>
+              <img
+                style={{ background: "white" }}
+                className="list"
+                src={medium}
+                alt=""
+              />
+            </li>
+          </a>
+        </ul>
+        <p className="intro">
+          Coded With &nbsp;
+          <HeartFilled style={{ color: "red" }} />
+          &nbsp; By PRAKASH
+        </p>
+      </div>
+    </footer>
+  );
+};
 
-export default Footer
+export default Footer;
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,17 +1,20 @@
 import { Affix } from "antd";
 import React from "react";
 import "./Header.css";
+import Prakash from '../../images/prakash.gif'
 
 const Header = () => {
   return (
     <Affix offsetTop={0}>
       <div id="header">
-        <div id="prakash">Prakash</div>
+        <div id="prakash">
+          <img style={{width:"5rem", borderRadius:"10px"}} src={Prakash} alt="" />
+        </div>
         <div id="about_prakash">
-          <ul id="lists">
-            <li className="list">Home</li>
-            <li className="list">Projects</li>
-            <li className="list">Blogs</li>
+          <ul id="lists_head">
+            <li className="list_head">Home</li>
+            <li className="list_head">Projects</li>
+            <li className="list_head">Blogs</li>
           </ul>
         </div>
       </div>
