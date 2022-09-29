a2a499ba9083d2e5309c288bdd8caf8290b17898
Prakash Agrahari <sshivamprakashh301@gmail.com>
2022-09-29 15:02:47 +0530

    resume updated

--- src/App.js
+++ src/App.js
@@ -1,19 +1,19 @@
-import './App.css';
-import Footer from './components/Footer/Footer';
-import Header from './components/Header/Header';
-import About from './Pages/About/About';
+import "./App.css";
+import Footer from "./components/Footer/Footer";
+import Header from "./components/Header/Header";
+import About from "./Pages/About/About";
 import { Switch, Route } from "react-router-dom";
-import Projects from './Pages/Projects/Projects';
-import Blogs from './Pages/Blogs/Blogs';
-import MyBlogs from './Pages/Blogs/MyBlogs';
-import Home from './Pages/Home/Home';
+import Projects from "./Pages/Projects/Projects";
+import Blogs from "./Pages/Blogs/Blogs";
+import MyBlogs from "./Pages/Blogs/MyBlogs";
+import Home from "./Pages/Home/Home";
 
 function App() {
   return (
     <div className="App">
       <Header />
       <Switch>
-      <Route exact path="/" component={Home} />
+        <Route exact path="/" component={Home} />
         <Route path="/about" component={About} />
         <Route path="/projects" component={Projects} />
         <Route path="/blogs" component={Blogs} />
