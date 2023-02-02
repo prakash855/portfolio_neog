6b346102e3d8c38c2a858286d696bc4481eb164a
prakash855 <sshivamprakashh301@gmail.com>
2023-02-02 14:33:49 +0530

    styles

--- src/components/Skills/Skill.jsx
+++ src/components/Skills/Skill.jsx
@@ -18,57 +18,71 @@ const Skill = () => {
     {
       name: "HTML5",
       icon: <AiFillHtml5 />,
+      color: "red",
     },
     {
       name: "CSS3",
       icon: <DiCss3 />,
+      color: "#04A7F4",
     },
     {
       name: "ANT DESIGN",
       icon: <AiOutlineAntDesign />,
+      color: "#71C1FF",
     },
     {
       name: "MATERIAL UI",
       icon: <SiMaterialUi />,
+      color: "#027FFE",
     },
     {
       name: "STYLED-COMPONENT",
       icon: <SiStyledComponents />,
+      color: "#FFA0E5",
     },
     {
       name: "JAVASCRIPT",
       icon: <SiJavascript />,
+      color: "#E8D44D",
     },
     {
       name: "REACT JS",
       icon: <SiReact />,
+      color: "#61D9FB",
     },
     {
       name: "REDUX",
       icon: <SiRedux />,
+      color: "#7248B6",
     },
     {
       name: "IONIC REACT",
       icon: <SiIonic />,
+      color: "#000",
     },
     {
       name: "GITHUB",
       icon: <AiFillGithub />,
+      color: "#0E1117",
     },
     {
       name: "NETLIFY",
       icon: <SiNetlify />,
+      color: "#31B4BA",
     },
     {
       name: "NPM",
       icon: <SiNpm />,
+      color: "red",
     },
   ];
   return (
     <div style={{ display: "flex", flexWrap: "wrap", marginTop: "1rem" }}>
       {skills.map((skill, id) => (
         <span key={id} className="tech">
-          <span style={{ display: "flex" }}>{skill.icon}</span>
+          <span style={{ display: "flex", color: skill.color }}>
+            {skill.icon}
+          </span>
           <code className="code">{skill.name}</code>
         </span>
       ))}
